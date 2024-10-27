80b6a57ac743978ba0f3e3e3db1251b8c17809b0
Jason Ho <git@hogt.me>
2024-10-27 00:46:32 -0700

    add mac setting change to prevent sleep so we can keep working on a different computer with lid closed, change function to alias

--- README.md
+++ README.md
@@ -11,15 +11,14 @@ Must be built to be used, see Installing.
 ## Installing
 In your relevant profile script (e.g. macOS: `~/.zprofile`) add a function to reference wherever you cloned this repo:
 ```zsh
-function kali-here() {
-    zsh ~/path/to/kali-repo/kali.sh
-}
+alias kali-here="zsh ~/path/to/kali-repo/kali.sh"
 ```
-You can also rename the function to whatever you'd like.
+You can also rename the alias to whatever you'd like, and change `zsh` to suit your local system (e.g. `bash`).
 
 ## Usage
 To use the tools, just run `kali-here` (or relevant command name) to start a Kali workspace in your current shell's directory and a remote desktop should be opened in your browser. The current directory will be mounted to the container's `/workspace` and `~/Desktop/workspace`, and should show up on the screen that opened.
 
 If a browser doesn't open to your Kali container, open `localhost:3000` in a browser.
 
-To close the container, just `CTRL+C` in the logs. The container should also be removed and restarted on every run of `kali-here`.
+~~To close the container, just `CTRL+C` in the logs. The container should also be removed and restarted on every run of `kali-here`.~~
+To close the container for now, run `kali-here kill`, it is planned for the future for just `CTRL+C` to terminate the Kali container and cleanup and settings we changed in the system.
--- kali.sh
+++ kali.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+MAC_DONTSLEEPONCHARGER() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "(macOS) Setting your computer to not sleep on charger (you can revert these settings to your desired value in Settings > Battery > Options > Prevent automatic sleeping on power adapter when the display is off > Set to Off)"
+        sudo pmset -c sleep 0
+    fi
+}
+
+MAC_SLEEPONCHARGER() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "(macOS) Setting your computer to not sleep on charger (you can revert these settings to your desired value in Settings > Battery > Options > Prevent automatic sleeping on power adapter when the display is off)"
+        sudo pmset -c sleep 1
+    fi
+}
+
 KILL_CONTAINER() {
   echo "Killing Kali container..."
   docker rm -f kali-linux || true
@@ -8,18 +22,26 @@ KILL_CONTAINER() {
 
 KILL() {
   echo "Exiting..."
+  MAC_SLEEPONCHARGER
   KILL_CONTAINER
   exit 0
 }
+
+echo "$0 $1 - $*"
+if [[ "$1" == "kill" ]]; then
+    KILL
+    echo "Exiting after removing container."
+    exit 0
+else
+    KILL_CONTAINER
+fi
+
+CONTAINER="kali-local"
 
 if [ ! -z $BUILD_KALI ]; then
     echo "BUILD_KALI specified- running Kali container build..."
     bash ~/Documents/kali/build.sh
 fi
-
-CONTAINER="kali-local"
-
-KILL_CONTAINER
 
 trap KILL SIGINT
 echo "Running kali at $PWD"
@@ -54,9 +76,10 @@ if [ -z $NOOPEN ]; then
     open $URL
 fi
 
+MAC_DONTSLEEPONCHARGER
 docker logs -f -n 999999 kali-linux
-
 # TODO: fix docker logs exiting the script above here
+MAC_SLEEPONCHARGER
 
 echo "Exiting Kali container.."
 KILL
